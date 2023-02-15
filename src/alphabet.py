57028cf64cf5d20302777bcc18cac489ad42180c
Marek Dohnal <datgaspkitteh@gmail.com>
2023-02-16 00:20:44 +0100

    First version of parser: img -> grid

---
+++ src/alphabet.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Alphabet(Enum):
+    IMG_BORDER = 100,
+    TILE_BORDER = 0,
+    FILL = 150,
+    FILL_BEGIN = 100,
+    SHAPE = 50,
+    EMPTY = 255
+
---
+++ test/parser_test.py
@@ -0,0 +1,9 @@
+import parser
+import cv2
+
+
+grid = parser.img_to_grid("../resources/test_grid.png")
+print(grid)
+# grid = cv2.resize(grid, (960, 540))
+# cv2.imshow('Binary Threshold Image', grid)
+# cv2.waitKey()
