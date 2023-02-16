86882068ea1f8c317cb3d6e9c926c74776270094
Marek Dohnal <datgaspkitteh@gmail.com>
2023-02-16 21:12:24 +0100

    Added comments to parser, created recognition function skeletons.

--- src/parser.py
+++ src/parser.py
@@ -4,6 +4,9 @@ from alphabet import Alphabet
 
 
 def img_to_grid(img_path: str):
+    """ Returns a numpy array of numbers representing a padded grid ready for recognition
+        from an input image.
+    """
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
     ret, bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
     grid = np.pad(array=bw_img, mode='constant', constant_values=Alphabet.IMG_BORDER.value, pad_width=((1, 1), (1, 1)))
---
+++ src/recognition.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def recognize_tiles(grid):
+    """ Recognizes tiles from an input grid and returns them as a 2D array. """
+    pass
+
+
+def find_new_beginning(grid, curr_pos):
+    """ Finds the beginning of a new tile based on a grid and current position. """
+    pass
+
+
+def fill_down(grid, fill_buffer):
+    """ Fills the tile down in one layer. Returns modified fill buffer, and end position. """
+    pass
+
+
+def fill_up(grid, fill_buffer):
+    """ Fills tile up based on input grid. Returns modified fill buffer, and end position. """
+    pass
+
+
+def find_new_fill_beginning(grid, curr_pos):
+    """ Finds new fill beginning (F_B) based on grid and current position """
+    pass
+
+
+def fill_tile(grid, curr_tile):
+    """ Fills the current tile, returns modified grid with filled tile and
+        inserts a new tile into the tile array.
+    """
+    pass
