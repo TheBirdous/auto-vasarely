fe4bf3e36be73c7afd11256ce1b135639ba18d59
Marek Dohnal <datgaspkitteh@gmail.com>
2023-03-21 21:00:24 +0100

    First version of the main module completed

--- src/auto_vasarely.py
+++ src/auto_vasarely.py
@@ -13,6 +13,9 @@ import shapes
 import cv2
 import os
 
+
+directions = ["U", "D", "L", "R", "UL", "UR", "DL", "DR"]
+
 arg_parser = argparse.ArgumentParser(prog='auto-vasarely',
                                      description='Fills and trasforms an input grid, which is specified by its path.')
 
@@ -23,7 +26,8 @@ arg_parser.add_argument('grid_path',
 arg_parser.add_argument('row_sequence',
                         type=str,
                         help='Describes the pattern created by changing number of tiles on rows of the input grid. '
-                             'Row lengths are not separated, pattern of lengths a, b, c would be simply written as abc '
+                             'Row lengths are not separated in the row_sequence argument, '
+                             'pattern of lengths a, b, c would be simply written as abc '
                              'Example: '
                              'a grid with 7 tiles on a row would have 7 for sequence, '
                              'whereas a grid with a pattern of 5 tiles per row followed by '
@@ -39,23 +43,25 @@ arg_parser.add_argument('background_colors_path',
 
 arg_parser.add_argument('output_path',
                         type=str,
-                        help='Folder into which the output is saved. ')
+                        help='Path represents a folder into which the output is saved. ')
 
 arg_parser.add_argument('-b', '--border_color_path',
                         type=str,
-                        help='Optional path to an image containing a color which will be applied to tile borders. '
+                        metavar="PATH",
+                        help='Optional PATH to an image containing a color which will be applied to tile borders. '
                              'Color with the highest occurence is used. '
                              'Borders are removed if this argument is not used.')
 
 arg_parser.add_argument('-c', '--shape_colors_path',
                         type=str,
-                        help='Optional argument containing a path to an image which is '
+                        metavar="PATH",
+                        help='Optional argument containing a PATH to an image which is '
                              'extracted into a palette used for shapes')
 
 arg_parser.add_argument('-s', '--shapes',
                         type=str,
                         help='Describes the inner shapes of tiles to be drawn to the output image. '
-                             'Shapes are specified as a sequence containing one of the following letters '
+                             'SHAPES are specified as a sequence containing one of the following letters '
                              'in any order: RUDS. The letters choose the following shapes: '
                              'U - Triangle rotated up, '
                              'D - Triangle rotated down, '
@@ -67,36 +73,91 @@ arg_parser.add_argument('-n', '--number_of_transformations',
                         type=int,
                         default=5,
                         help='Optional argument specifying the '
-                             'number of transformations to be applied to the tile grid. Default value is 5.')
+                             'NUMBER_OF_TRANSFORMATIONS to be applied to the tile grid. Default value is 5.')
 
 arg_parser.add_argument('-st', '--sort_types',
                         action='store_true',
+                        help='Optional argument specifying whether shapes are sorted according to their types.'
+                             'If set, the sorting pattern applied is: triangles pointed up: UP, triangles pointed'
+                             'down: DOWN, squares: LEFT, rotated squares: RIGHT')
+
+arg_parser.add_argument('-ss', '--smaller_shape_dir',
+                        type=str,
+                        default="U",
+                        choices=directions,
+                        metavar="DIRECTION",
                         help='Optional argument specifying the '
-                             'number of transformations to be applied to the tile grid. Default value is 5.')
+                             'DIRECTION smaller shapes move up in when transformations are executed. '
+                             'Default value is U. ')
+
+arg_parser.add_argument('-lb', '--lighter_background_dir',
+                        type=str,
+                        default="U",
+                        choices=directions,
+                        metavar="DIRECTION",
+                        help='Optional argument specifying the '
+                             'DIRECTION in which lighter background move up when transformations are executed. '
+                             'Default value is UL. ')
+
+arg_parser.add_argument('-ls', '--lighter_shape_dir',
+                        type=str,
+                        default="U",
+                        choices=directions,
+                        metavar="DIRECTION",
+                        help='Optional argument specifying the '
+                             'DIRECTION in which lighter shapes move up when transformations are executed. '
+                             'Default value is DR. ')
 
 arg_parser.add_argument('-o', '--output_scale',
-                        type=int,
+                        type=float,
                         default=1,
+                        metavar="SCALE",
                         help='Optional argument which changes the scale relative to an output image. Default is 1 '
-                             'values > 1 enlarge the resolution, smaller values shrink it.')
-
+                             'values > 1 enlarge the resolution, smaller values shrink it. '
+                             'Argument must be in the interval (0, 100]')
 
 args = arg_parser.parse_args()
 
-print("Parsing image...")
-grid = parser.img_to_grid(args.grid_path)
+# Argument check
 
-print("Recognizing grid...")
-int_seq_list = [int(c) for c in list(args.row_sequence)]
-tile_grid = recognition.recognize_tiles(grid, int_seq_list)
+err_found = False
+if not os.path.exists(args.grid_path):
+    print(f"ERROR: Invalid grid_path, please specify a correct path for the input grid.")
+    err_found = True
 
-print("Extracting colors...")
-bg_colors = palette.img_to_palette(args.background_colors_path)
+if not os.path.exists(args.background_colors_path):
+    print(f"ERROR: Invalid background_colors_path, please specify a correct path for the image containing"
+          f"background colors.")
+    err_found = True
 
-if args.shape_colors_path is None:
-    shape_colors = bg_colors
-else:
-    shape_colors = palette.img_to_palette(args.shape_colors_path)
+if not os.path.exists(args.output_path):
+    print(f"ERROR: Invalid output_path, please specify a correct path for the output images and video.")
+    err_found = True
+
+if args.border_color_path is not None:
+    if not os.path.exists(args.border_color_path):
+        print(f"ERROR: Invalid PATH in --border_colors_path (-b), please specify a correct path for the image "
+              f"containing border colors.")
+        err_found = True
+
+if args.shape_colors_path is not None:
+    if not os.path.exists(args.shape_colors_path):
+        print(f"ERROR: Invalid PATH in --shape_colors_path (-c), please specify a correct path for the image "
+              f"containing shape colors.")
+        err_found = True
+
+try:
+    int_seq_list = [int(c) for c in list(args.row_sequence)]
+    for digit in int_seq_list:
+        if digit <= 0:
+            print(f"ERROR: The row_sequence argument can only contain digits larger than 0. "
+                  f"Please enter a correct sequence or use the -h flag to see correct usage.")
+            err_found = True
+            break
+except ValueError:
+    print(f"ERROR: The row_sequence argument can only contain digits larger than 0. "
+          f"Please enter a correct sequence or use the -h flag to see correct usage.")
+    err_found = True
 
 if args.shapes is None:
     shape_list = [shapes.Shapes.CONTOUR]
@@ -107,20 +168,70 @@ else:
         'S': shapes.Shapes.SQUARE,
         'R': shapes.Shapes.SQUARE_45DEG
     }
-    shape_list = [shape_dict[c] for c in list(args.shapes)]
+    try:
+        args.shapes = args.shapes.upper()
+        shape_list = [shape_dict[c] for c in list(args.shapes)]
+    except KeyError:
+        print(f"ERROR: The optional argument --shapes (-s) can only contain characters "
+              f"R, U, D, S.")
+        err_found = True
+
+
+if not (0 < args.output_scale <= 100):
+    print(f"ERROR: The optional argument --output_scale (-o) has an incorrect value of {args.output_scale}."
+          f"Argument must be in the interval (0, 100].")
+    err_found = True
+
+if err_found:
+    quit()
+
+frames_path = os.path.join(args.output_path, "frames")
+if not os.path.exists(frames_path):
+    # if the demo_folder directory is not present
+    # then create it.
+    os.makedirs(frames_path)
+
+print(args)
+print(os.path.join(args.output_path, "video.avi"))
+
+# Execution the main body
+print("Parsing image...")
+grid = parser.img_to_grid(args.grid_path)
+
+print("Recognizing grid...")
+tile_grid = recognition.recognize_tiles(grid, int_seq_list)
+
+print("Extracting colors...")
+bg_colors = palette.img_to_palette(args.background_colors_path)
+
+if args.shape_colors_path is None:
+    shape_colors = bg_colors
+else:
+    shape_colors = palette.img_to_palette(args.shape_colors_path)
 
 print("Initializing tile grid...")
 tile_grid.init_tile_attributes(bg_colors, shape_colors, shape_list)
 shapes.init_shape_fill_templates(tile_grid)
 
-print(tile_grid)
+# print(tile_grid)
 imgs = []
 print("Converting to images...")
-for i in range(0, args.number_of_transformations):
+for i in range(0, args.number_of_transformations + 1):
     out_img = tile_grid.to_image(grid, args.border_color_path)
     imgs.append(out_img)
-    cv2.imwrite(os.path.join(args.output_path, f"frame_{i}.png"), out_img)
-    print(f"Applying transformation {i}...", end='\r')
-    tile_grid.apply_transformation_step("U", "UL", "L", True)
+    cv2.imwrite(os.path.join(frames_path, f"frame_{i}.png"), out_img)
+    print(f"Applying transformation {i} of total {args.number_of_transformations}...", end='\r')
+    tile_grid.apply_transformation_step(args.smaller_shape_dir,
+                                        args.lighter_background_dir,
+                                        args.lighter_shape_dir,
+                                        args.sort_types)
+print()
 print("Encoding video...")
-videoenc.imgs_to_video(os.path.join(args.output_path, "video.avi"), imgs, args.output_scale)
+video_path = os.path.join(args.output_path, "video.avi")
+videoenc.imgs_to_video(video_path, imgs, args.output_scale)
+print()
+print(f"Video was saved into the following file:")
+print(os.path.abspath(video_path))
+print(f"Frames were saved into the following folder:")
+print(os.path.abspath(frames_path))
+print()
